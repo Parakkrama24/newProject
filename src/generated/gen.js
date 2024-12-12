7d8e4504311f63377aaf40db2ab579bcd38236de
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-12-13 00:28:08 +0530

    Initial commit

---
+++ src/generated/gen.js
@@ -0,0 +1,16 @@
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+needle_exported_files.push("assets/vite.glb?v=1734029731300");
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1734029731300");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.51\.2");
