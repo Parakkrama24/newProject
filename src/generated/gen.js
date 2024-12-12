4b1266d4eff3ba5c5d9a5f367498adfd472ac2fe
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2024-12-13 01:20:45 +0530

    new

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("assets/Scene.glb?v=1734032403454");
+needle_exported_files.push("assets/vite.glb?v=1734033001515");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1734032403454");
+		needleEngine.setAttribute("hash", "1734033001515");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
